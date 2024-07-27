49eb857bd62a6a5ba74e00d52611489c739ee5b9
Heba <hebamagde@gmail.com>
2024-07-27 05:51:21 +0300

    update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -21,21 +21,21 @@ export default class HolbertonCourse {
   // Setters
   set name(name) {
     if (typeof name !== 'string') throw TypeError('Name must be a string');
-      this._name = name;
+    this._name = name;
   }
 
   set length(length) {
     if (typeof length !== 'number') throw TypeError('Name must be a number');
-      this._length = length;
+    this._length = length;
   }
 
   set students(students) {
     if (!Array.isArray(students)) {
-        throw new TypeError('Students must be an array');
-      }
-      if (students.some((element) => typeof element !== 'string')) {
-        throw new TypeError('Students must be an array of strings');
-      }
-    this._students = students;
+      throw new TypeError('Students must be an array');
+    }
+    if (students.some((element) => typeof element !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+  this._students = students;
   }
 }
