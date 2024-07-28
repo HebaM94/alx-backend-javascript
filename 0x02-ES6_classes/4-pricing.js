cf827637f8992daa1a05526f3f8465e3d99df734
Heba <hebamagde@gmail.com>
2024-07-28 22:58:56 +0300

    update 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,44 +1,44 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-      this.amount = amount;
-      this.currency = currency;
-    }
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-    // Getter
-    get amount() {
-      return this._amount;
-    }
+  // Getter
+  get amount() {
+    return this._amount;
+  }
 
-    get currency() {
-      return this._currency;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    // Setters
-    set amount(amount) {
-      if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
-      this._amount = amount;
-    }
+  // Setters
+  set amount(amount) {
+    if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
+    this._amount = amount;
+  }
 
-    set currency(currency) {
-      if (!(currency instanceof Currency)) throw new TypeError('Currency be an instance of Currency class');
-      this._currency = currency;
-    }
+  set currency(currency) {
+    if (!(currency instanceof Currency)) throw new TypeError('Currency be an instance of Currency class');
+    this._currency = currency;
+  }
 
-    // Functions
-    displayFullPrice() {
-      return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-    }
+  // Functions
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
 
-    static convertPrice(amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number') {
-        throw new TypeError('Amount must be a number');
+      throw new TypeError('Amount must be a number');
     }
 
     if (typeof conversionRate !== 'number') {
-        throw new TypeError('Conversion must be a number');
+      throw new TypeError('Conversion must be a number');
     }
     return amount * conversionRate;
-    }
+  }
 }
