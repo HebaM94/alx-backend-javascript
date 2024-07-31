44587093ec566cb5db0f68ab8b7c032ef261a8ea
Heba <hebamagde@gmail.com>
2024-07-31 04:47:14 +0300

    Task 9: Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function mapList() {
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomato', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+}
