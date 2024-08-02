b0d580ff349595074153f0f07329d4a0b659c5e1
Heba <hebamagde@gmail.com>
2024-08-02 22:18:43 +0300

    Tasks: 0 to 4

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -9,14 +9,40 @@ const studentOne: Student = {
     firstName: 'John',
     lastName: 'Doe',
     age: 20,
-    location: 'California'
+    location: 'London'
 }
 const studentTwo: Student = {
     firstName: 'Jane',
     lastName: 'Doe',
     age: 22,
-    location: 'New York'
+    location: 'Manchester'
 }
-const students: Student[] = [studentOne, studentTwo];
+const studentsList: Student[] = [studentOne, studentTwo];
 
+const table = document.createElement('table');
+table.style.border = '1px solid black';
 
+const tableHeader = document.createElement('tr');
+const firstNameHeader = document.createElement('th');
+const locationHeader = document.createElement('th');
+firstNameHeader.textContent = 'Name';
+locationHeader.textContent = 'Location';
+firstNameHeader.style.border = '1px solid black';
+locationHeader.style.border = '1px solid black';
+tableHeader.append(firstNameHeader, locationHeader);
+table.appendChild(tableHeader);
+
+studentsList.forEach((student) => {
+    const row = document.createElement('tr');
+    const firstName = document.createElement('td');
+    const location = document.createElement('td');
+    firstName.textContent = student.firstName;
+    location.textContent = student.location;
+    row.style.border = '1px solid black';
+    firstName.style.border = '1px solid black';
+    location.style.border = '1px solid black';
+    row.append(firstName, location);
+    table.append(row);
+});
+
+document.body.appendChild(table);
---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,39 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [index: string]: any;
+}
+
+interface Director extends Teacher{
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface{
+    constructor (public firstName: string, public lastName: string) {}
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
