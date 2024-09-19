b3debf9460d3476906aaab811d66f0e7a920dc2d
Heba <hebamagde@gmail.com>
2024-09-19 21:37:24 +0300

    Task 3: Spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const numOne = Math.round(a);
+    const numTwo = Math.round(b);
+    switch (type) {
+      case 'SUM':
+        return numOne + numTwo;
+      case 'SUBTRACT':
+        return numOne - numTwo;
+      case 'DIVIDE':
+        if (!numTwo) return 'Error';
+        return numOne / numTwo;
+      default:
+        throw new Error('Operation not supported. Type must be SUM, SUBTRACT or DIVIDE');
+    }
+  },
+};
+
+module.exports = Utils;
