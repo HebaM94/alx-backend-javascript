540a735b93973dcfa37954f8d2cb024ba455d8e7
Heba <hebamagde@gmail.com>
2024-09-19 21:43:49 +0300

    Task 6: Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  describe('#Async test', function() {
+    
+    it('should test response from payment API', function(done) {
+      getPaymentTokenFromAPI(true)
+        .then((res) => {
+          expect(res).to.be.an('object')
+            .with.property('data', 'Successful response from the API');
+          done();
+        });
+    });
+  });
+});
